642964505ce1a2671e201a19d3f7390b88bb00c9
Devil-2621 <tirth8000@gmail.com>
2025-09-03 14:32:07 +0530

    feat: add Inngest API integration & initial setup of background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import { caller } from "@/trpc/server";
+"use client";
 
-const Page = async () => {
-  const data = await caller.hello({ text: "Tirth" });
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
   return (
     <div>
-      {JSON.stringify(data)}
+      <Button onClick={() => invoke.mutate({ text: "Tirth Ladani" })}>
+        Invoke Background Job
+      </Button>
     </div>
   );
-}
+};
 
 export default Page;
