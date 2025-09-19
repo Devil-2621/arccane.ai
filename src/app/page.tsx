fa8ad3366e720e379626dbb5cfd3d2bb89910689
Devil-2621 <tirth8000@gmail.com>
2025-09-19 14:29:34 +0530

    feat: Implemented projectRouter and created initial projects page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,42 @@
 import { useState } from "react";
 import { toast } from "sonner";
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Niconne } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
 
 const Page = () => {
+  const router = useRouter();
   const [value, setValue] = useState("");
 
   const trpc = useTRPC();
-  const {data: messages} = useQuery(trpc.messages.getMany.queryOptions());
-  const createMessage = useMutation(
-    trpc.messages.create.mutationOptions({
-      onSuccess: () => {
-        toast.success("Message created");
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onError: (error) => {
+        toast.error(error.message);
+      },
+      onSuccess: (data) => {
+        toast.success("Project created");
+        router.push(`/projects/${data.id}`); // Redirect to the new project page
       },
     })
   );
 
   return (
-    <div>
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button
-        onClick={() => createMessage.mutate({ value: value })}
-        disabled={createMessage.isPending}
-      >
-        Invoke Background Job
-      </Button>
-      {JSON.stringify(messages, null, 2)}
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="max-w-7xl mx-auto flex flex-col gap-y-4 items-center justify-center">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          onClick={() => createProject.mutate({ value: value })}
+          disabled={createProject.isPending}
+        >
+          Submit
+        </Button>
+        <Toaster />
+      </div>
     </div>
   );
 };
--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -18,13 +18,17 @@ export const messagesRouter = createTRPCRouter({
     create: baseProcedure
     .input(
         z.object({
-            value: z.string().min(1, {message: "Message is required "}),
+            value: z.string()
+                .min(1, {message: "Value is required "})
+                .max(10000, { message: "Value must be less than 10000 characters" }),
+            projectId: z.string().min(1, { message: "Project ID is required" }),
         }),
     )
     .mutation(async ({ input }) => {
         const createdMessage = await prisma.message.create
         ({
             data:{
+                projectId: input.projectId,
                 content: input.value,
                 role: "USER",
                 type: "RESULT",
@@ -36,11 +40,10 @@ export const messagesRouter = createTRPCRouter({
           name: "coding-agent/run",
           data: {
             value: input.value,
+            projectId: input.projectId,
           }
         });
 
         return createdMessage;
     }),
 });
-
-//message.create()
